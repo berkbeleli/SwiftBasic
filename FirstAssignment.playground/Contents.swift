56a9204318a0c94a04cb11fe1c6d776c83b6ca8a
Berk <berkbeleli@gmail.com>
2022-05-19 23:45:32 +0300

    Order summary function for CupcakeOrder struct
    Created displayOrderSummary function to display user's order summary

--- FirstAssignment.playground/Contents.swift
+++ FirstAssignment.playground/Contents.swift
@@ -41,7 +41,44 @@ struct CupcakeOrder{
     if addSprinkles{
       self.cost! += Double(quantity) / 2
     }
-
+    
   }
+  
+  /// <#Description#>
+  /// This function gives the summary of the choices and cost of the value
+  ///
+  /// ```
+  /// var firstOrder = CupcakeOrder(type: 1, quantity: 4, addSprinkles: false)
+  /// print(firstOrder.displayOrderSummary())
+  /// ```
+  ///
+  /// - Warning: The returned string is not localized. before this function do not forget the call calculateCost function!
+  /// - Returns: A summary string
+  func displayOrderSummary() -> String{
+    //Check if quantity has valid value or not
+    guard let quantity = quantity else {
+      return "Unvalid quantity"
+    }
+    //Check if cost has valid value or not
+    guard let cost = cost else {
+      return "Unvalid cost"
+    }
+    
+    if quantity <= 0 { //  this line of code checks if quantity set to zero or minus value
+      return "Add some cupcake to your cart"
+    }else if self.type > CupcakeOrder.cakeTypes.count{ // this line ckecks if type value is valid
+      return "Invalid Cupcake Type"
+    }else if cost > 0{ // if the 2 if statement passed and its cost calculated it will summarize order and return multiline string
+      return """
+       You ordered cupcake with \(CupcakeOrder.cakeTypes[self.type])
+       amount of \(self.quantity ?? 0)
+       \(self.addSprinkles ? "with Sprinkles" : "without Sprinkles")
+       and costs $\(cost)
+       """
+    }else{ // if none of the upper if statement validated this linewe will work
+      return "Something went wrong try again"
+    }
+  }
+  
   
 }
