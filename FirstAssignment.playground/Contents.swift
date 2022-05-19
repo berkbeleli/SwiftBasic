5e9af5dbd681f0d897c7be12534d268f6ed863b1
Berk <berkbeleli@gmail.com>
2022-05-19 23:37:45 +0300

    New functionality for CupcakeOrder struct
    Added calculateCost method to for cost calculation

--- FirstAssignment.playground/Contents.swift
+++ FirstAssignment.playground/Contents.swift
@@ -20,6 +20,28 @@ struct CupcakeOrder{
   //Make cost value private because we don't want anyone to reachout our cost value and change it
   fileprivate var cost: Double? = nil
   
+  /// <#Description#>
+  /// This function calculates the value of cost for the given order according to the choices
+  ///
+  ///  ```
+  /// var firstOrder = CupcakeOrder(type: 1, quantity: 4, addSprinkles: false)
+  /// firstOrder.calculateCost()
+  /// ```
+  ///
+  mutating func calculateCost(){ // make our function mutating as we change self struct variable inside
+    guard let quantity = quantity else {
+      self.cost = 0
+      return
+    }
+    
+    self.cost = Double(quantity) * 2
+    self.cost! += (Double(type) / 2)
+    
+    //0.50$ for sprinkles
+    if addSprinkles{
+      self.cost! += Double(quantity) / 2
+    }
 
+  }
   
 }
